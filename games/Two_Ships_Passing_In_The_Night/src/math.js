143a4c7e083d7ea58fdaf9e98ca7180c386c962f
Baussart Renaud <renaud.baussart@proton.me>
2026-04-13 10:20:44 +0200

    fix(math.js): fix export duplicate

--- games/Two_Ships_Passing_In_The_Night/src/math.js
+++ games/Two_Ships_Passing_In_The_Night/src/math.js
@@ -1,20 +1,15 @@
 export var clamp = (value, min, max) => Math.min(Math.max(value, min), max);
 
-export var lerp = (x, y, t) => (1 - t) * x + t * y;
-
-export var mapLinear = (x, a1, a2, b1, b2) =>
-  b1 + ((x - a1) * (b2 - b1)) / (a2 - a1);
-
-export var randFloat = (low, high) => low + Math.random() * (high - low);
-
-export var randFloatSpread = range => range * (0.5 - Math.random());
-
 export function lerp(x, y, a) {
   return x + (y - x) * a;
 }
+
 export function mapLinear(x, a1, a2, b1, b2) {
   return b1 + ((x - a1) * (b2 - b1)) / (a2 - a1);
 }
+export var randFloat = (low, high) => low + Math.random() * (high - low);
+
+
 export function randFloatSpread(range) {
   return range * (0.5 - Math.random());
 }
