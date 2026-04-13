3640808f27afe89ef08819bc8a05b508fcfaa1b6
Baussart Renaud <renaud.baussart@proton.me>
2026-04-13 17:16:34 +0200

    feat: add a timeout to playwright

--- games/Two_Ships_Passing_In_The_Night/playwright.config.js
+++ games/Two_Ships_Passing_In_The_Night/playwright.config.js
@@ -1,13 +1,15 @@
 import {defineConfig} from '@playwright/test';
 
 export default defineConfig({
-  testDir: './tests',
+  testDir: './src/tests',
   use: {
-    browserName: 'chromium',
+    baseURL: 'http://127.0.0.1:5173',
+    screenshot: 'only-on-failure',
   },
   webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:3000/',
+    command: 'npx vite preview --port 5173 --host 0.0.0.0',
+    url: 'http://127.0.0.1:5173',
     reuseExistingServer: !process.env.CI,
+    timeout: 180 * 1000,
   },
 });
