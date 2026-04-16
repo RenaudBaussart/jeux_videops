e0f4b57360a7e654ff4dfbd05f8b06e7b6e6915d
Baussart Renaud <renaud.baussart@proton.me>
2026-04-16 16:56:50 +0200

    fix: change vite config

--- games/Two_Ships_Passing_In_The_Night/vite.config.js
+++ games/Two_Ships_Passing_In_The_Night/vite.config.js
@@ -1,6 +1,3 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  // on force vite a utiliser des chemins relatifs pour fonctionner dans un sous-dossier
+export default {
   base: './'
-});
+}
